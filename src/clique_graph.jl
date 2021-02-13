dd83a2e6ffd7935f56f52de3e5b2e1bf07596a4c
Theo Diamandis <tjdiamandis@gmail.com>
2021-02-13 18:15:26 -0500

    trees are out; graphs are in

---
+++ src/clique_graph.jl
@@ -0,0 +1,74 @@
+using SimpleWeightedGraphs: SimpleWeightedGraph, add_edge!
+using MetaGraphs: MetaGraph, set_prop!, get_prop
+
+function generate_clique_graph(cliques)
+    N = length(cliques)
+    cliques = [Set(c) for c in cliques]
+    # G = SimpleWeightedGraph(N)
+    G = MetaGraph(N)
+    for i in 1:N
+        set_prop!(G, i, :nodes, cliques[i])
+    end
+    for i in 1:N, j in i+1:N
+        # TODO: iterate through for performance?
+        l_int = length(intersect(cliques[i], cliques[j]))
+        if l_int > 0
+            w_ij = weight_function(cliques[i], cliques[j], l_int)
+            # add_edge!(G, i, j, w_ij)
+            add_edge!(G, i, j)
+            set_prop!(G, i, j, :weight, w_ij)
+        end
+    end
+    return G
+end
+
+
+function weight_function(c_i, c_j, l_int)
+    l_ci = length(c_i)
+    l_cj = length(c_j)
+    l_union = l_ci + l_cj - l_int
+    return l_ci^3 + l_cj^3 - l_union^3
+end
+
+
+function _merge_cliques!(cg, i, j)
+    new_clique = union(get_prop(cG, i, :nodes), get_prop(cG, i, :nodes))
+    n_rem = degree(cG, i) < degree(cG, j) ? i : j
+    n_keep = degree(cG, i) < degree(cG, j) ? j : i
+
+    set_prop!(G, n_keep, :nodes, new_clique)
+
+    rem_edge!(cg, i, j)
+    for neighbor in union(neighbors(cg, n_rem), neighbors(cg, n_keep))
+        # Some redundancy here. Idk if it matters much
+        rem_edge!(cg, n_rem, neighbor)
+        add_edge!(cg, neighbor, n_keep)
+
+        # Update weight
+        l_int = length(intersect(new_clique, get_prop(cG, neighbor, :nodes)))
+        w_ij = weight_function(new_clique, get_prop(cG, neighbor, :nodes), l_int)
+        set_prop!(cg, neighbor, n_keep, :weight, w_ij)
+    end
+    rem_vertex!(cg, n_rem)
+end
+
+
+function merge_cliques(cg)
+    max_val, max_ind = findmax([get_prop(cG, e, :weight) for e in edges(cG)])
+
+    while max_val > 0
+        i, j = get_edge(max_ind, edges(cG))
+        _merge_cliques!(cg, i, j)
+
+        max_val, max_ind = findmax([get_prop(cG, e, :weight) for e in edges(cG)])
+    end
+end
+
+
+function get_edge(max_ind, edge_iter)
+    count = 1
+    for e in edge_iter
+        count == max_ind && return src(e), dst(e)
+        count += 1
+    end
+end
