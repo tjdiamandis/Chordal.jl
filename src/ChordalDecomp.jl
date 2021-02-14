7e7728f86184f905fc8ac06dc0f4062d6536ab47
Theo Diamandis <tjdiamandis@gmail.com>
2021-02-13 23:01:34 -0500

    export functions

--- src/ChordalDecomp.jl
+++ src/ChordalDecomp.jl
@@ -6,5 +6,13 @@ using DataStructures
 
 
 include("chordal_extension.jl")
+include("clique_graph.jl")
+include("chordal_decomp.jl")
+include("utils.jl")
+
+export preprocess!, sparsity_pattern, get_chordal_extension, get_cliques
+export generate_clique_graph, merge_cliques!, get_cliques
+export make_selectors
+export build_perm_matrix
 
 end
