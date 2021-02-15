66541fed215009d424e75b7a2c85b9d34aed760b
Theo Diamandis <tjdiamandis@gmail.com>
2021-02-15 18:34:16 -0500

    add random sdp function

--- src/ChordalDecomp.jl
+++ src/ChordalDecomp.jl
@@ -13,6 +13,6 @@ include("utils.jl")
 export preprocess!, sparsity_pattern, get_chordal_extension, get_cliques
 export generate_clique_graph, merge_cliques!, get_cliques
 export get_selectors, make_selectors_from_cliques
-export build_perm_matrix
+export build_perm_matrix, generate_random_sdp
 
 end
--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,5 @@
+using Random
+
 function build_perm_matrix(p)
     isnothing(p) && return I
     n = length(p)
@@ -6,4 +8,30 @@ function build_perm_matrix(p)
         P[i, p[i]] = 1.0
     end
     return P
+end
+
+
+# Build from KKT conditions:
+# # KKT Conditions
+# Fx = sum(F[i]*xstar[i] for i in 1:n) + G
+# all(eigvals(Matrix(Fx)) .>= 0)
+# all(eigvals(D) .>= 0)
+# all([tr(F[i]*D) .== c[i] for i in 1:n])
+# tr(Fx*D) .== 0
+function generate_random_sdp(n; rand_seed=0)
+    Random.seed!(rand_seed)
+    D = diagm(1 .+ rand(n))
+    F = Vector{SparseMatrixCSC{Float64}}(undef, n)
+    c = Vector{Float64}(undef, n)
+    for i in 1:n
+        F[i] = spzeros(n, n)
+        block_size = randn() > 1 ? 2 : 10
+        F[i][i:min(i+block_size,n), i:min(i+block_size,n)] .= 1
+        c[i] = tr(D*F[i])
+    end
+    xstar = rand(n)
+    Fx = sum(F[i]*xstar[i] for i in 1:n)
+    G = -Fx
+
+    return c, F, G, xstar, D
 end
