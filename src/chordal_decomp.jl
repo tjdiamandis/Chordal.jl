d7a60168bbae80397c6f1a9ea07fb6f7091ce30e
Theo Diamandis <tjdiamandis@gmail.com>
2021-02-14 22:58:53 -0500

    add function to get generator matrices from matrix input

--- src/chordal_decomp.jl
+++ src/chordal_decomp.jl
@@ -1,4 +1,4 @@
-function make_selectors(cliques, n)
+function make_selectors_from_cliques(cliques, n)
     selector_mats = [spzeros(length(cliques[i]), n) for i in 1:length(cliques)]
     for i in 1:length(cliques)
         idx = 1
@@ -8,4 +8,22 @@ function make_selectors(cliques, n)
         end
     end
     return selector_mats
+end
+
+
+function get_selectors(input_mat::SparseMatrixCSC)
+    n = size(input_mat)[1]
+    preprocess!(input_mat)
+
+    sp = sparsity_pattern(input_mat)
+    P, L = get_chordal_extension(sp)
+    P = build_perm_matrix(P)
+
+    cliques = get_cliques(L)
+    cg = generate_clique_graph(cliques)
+    merge_cliques!(cg; verbose=true)
+
+    Cℓs = get_cliques(cg)
+    Tℓs = make_selectors_from_cliques(Cℓs, n)
+    return P, Cℓs, Tℓs
 end
