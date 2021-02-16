8f29fc1b3a4a3bcaf70ae8ab0a6e5b1f72de5471
Theo Diamandis <tjdiamandis@gmail.com>
2021-02-15 19:14:57 -0500

    cleanup

--- examples/sdp_mwe.jl
+++ examples/sdp_mwe.jl
@@ -5,11 +5,12 @@ using MosekTools
 using SparseArrays, JuMP, LinearAlgebra, Random
 
 
-rand_seed = 123
-
-## Without decomposition
+rand_seed = 1234
 n = 200
 c, F, G, xstar, D = generate_random_sdp(n, rand_seed=rand_seed)
+
+
+## Without decomposition
 m = Model(optimizer_with_attributes(
     Mosek.Optimizer,
     "QUIET" => false,
@@ -17,7 +18,7 @@ m = Model(optimizer_with_attributes(
 @variable(m, x[1:n])
 @constraint(m, sum(F[i]*x[i] for i in 1:n) + G ∈ PSDCone())
 @objective(m, Min, dot(c, x))
-time_non_chordal = @timed optimize!(m)
+time_non_chordal = @elapsed optimize!(m)
 
 xv = value.(x)
 pstar = dot(c,xv)
@@ -27,7 +28,7 @@ pstar = dot(c,xv)
 @info "Optimal value: $pstar"
 
 
-## Decomposition Setup
+## Chordal Decomposition Setup
 function build_constraints!(m, A)
     P, Cℓs, Tℓs = get_selectors(A)
     A = P*A*P'
@@ -50,16 +51,18 @@ m2 = Model(optimizer_with_attributes(
 A = dropzeros(sum(F[i]*y[i] for i in 1:n) + G)
 nnzA = nnz(A)
 @info "There are $nnzA nonzeros; density = $(round(nnzA/n^2, digits=3))"
-time_constraints = @timed build_constraints!(m2, A)
-@info "Finished adding constraints, time = $(time_constraints.time)s"
+time_constraints = @elapsed build_constraints!(m2, A)
+@info "Finished adding constraints, time = $(round(time_constraints, digits=3))s"
 @objective(m2, Min, dot(c,y))
 @info "Finished setup"
-## do things™
-time_chordal = @timed optimize!(m2)
 
+
+## do things™
+time_chordal = @elapsed optimize!(m2)
 yv = value.(y)
 pstar_chordal = dot(c,yv)
+
 @info "termination status: $(termination_status(m))"
 @info "solution status: $(primal_status(m))"
 @info "difference with non-chordal: $(norm(yv - xv))"
-@info "Time with chordal is $(time_chordal.time)s vs $(time_non_chordal.time)s"
+@info "Time with chordal is $(round(time_chordal, digits=3))s vs $(round(time_non_chordal, digits=3))s"
