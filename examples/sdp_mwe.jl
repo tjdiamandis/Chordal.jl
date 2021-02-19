903b868f9184d8ccd3c9abef33c85be9c82caac3
Theo Diamandis <tjdiamandis@gmail.com>
2021-02-18 20:16:58 -0500

    update to new interface

--- examples/sdp_mwe.jl
+++ examples/sdp_mwe.jl
@@ -1,12 +1,12 @@
 cd(joinpath(@__DIR__, ".."))
 Pkg.activate(".")
 using ChordalDecomp
-using MosekTools
-using SparseArrays, JuMP, LinearAlgebra, Random
+using JuMP, MosekTools, Hypatia
+using SparseArrays, LinearAlgebra, Random
 
 
 rand_seed = 1234
-n = 200
+n = 100
 c, F, G, xstar, D = generate_random_sdp(n, rand_seed=rand_seed)
 
 
@@ -15,6 +15,7 @@ m = Model(optimizer_with_attributes(
     Mosek.Optimizer,
     "QUIET" => false,
 ))
+# m = Model(Hypatia.Optimizer)
 @variable(m, x[1:n])
 @constraint(m, sum(F[i]*x[i] for i in 1:n) + G ∈ PSDCone())
 @objective(m, Min, dot(c, x))
@@ -29,6 +30,7 @@ pstar = dot(c,xv)
 
 
 ## Chordal Decomposition Setup
+# A + S ∈ PSDCone()
 function build_constraints!(m, A)
     P, Cℓs, Tℓs = get_selectors(A)
     A = P*A*P'
@@ -43,12 +45,39 @@ function build_constraints!(m, A)
     end
 end
 
+
+function build_A(F, G, y)
+    n = length(y)
+    A = spzeros(GenericAffExpr{Float64, VariableRef}, n, n)
+    for (j, k, v) ∈ zip(findnz(sparsity_pattern(F..., G))...)
+        A[j,k] = 1
+    end
+
+    for i in 1:n
+        for (j, k, v) ∈ zip(findnz(F[i])...)
+            add_to_expression!(A[j,k], F[i][j,k], y[i])
+        end
+    end
+    for (j, k, v) ∈ zip(findnz(G)...)
+        add_to_expression!(A[j,k], G[j,k])
+    end
+
+    for (j, k, v) ∈ zip(findnz(sparsity_pattern(F..., G))...)
+        A[j,k] -= 1
+    end
+    return A
+end
+
+
+
+
 m2 = Model(optimizer_with_attributes(
     Mosek.Optimizer,
     "QUIET" => false,
 ))
 @variable(m2, y[1:n])
-A = dropzeros(sum(F[i]*y[i] for i in 1:n) + G)
+A = build_A(F, G, y)
+# sum(F[i]*y[i] for i in 1:n) + G
 nnzA = nnz(A)
 @info "There are $nnzA nonzeros; density = $(round(nnzA/n^2, digits=3))"
 time_constraints = @elapsed build_constraints!(m2, A)
@@ -65,4 +94,5 @@ pstar_chordal = dot(c,yv)
 @info "termination status: $(termination_status(m))"
 @info "solution status: $(primal_status(m))"
 @info "difference with non-chordal: $(norm(yv - xv))"
+@info "difference with true variable: $(norm(yv - xstar))"
 @info "Time with chordal is $(round(time_chordal, digits=3))s vs $(round(time_non_chordal, digits=3))s"
