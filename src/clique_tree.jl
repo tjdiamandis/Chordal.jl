e8fa6aec6626cd99271fec3c6e5058116452612d
Theo Diamandis <tjdiamandis@gmail.com>
2021-06-23 17:11:16 -0400

    add nonalloc version for performance

--- src/clique_tree.jl
+++ src/clique_tree.jl
@@ -1,22 +1,29 @@
-
+"""
+    etree(A)
+Compute the elimination tree of a symmetric sparse matrix `A` from `triu(A)`.
+"""
 function etree(A::SparseMatrixCSC{Tv, Ti}) where {Tv <: AbstractFloat, Ti <: Integer}
+    !issymmetric(A) && throw(ArgumentError("Matrix must be symmetric"))
     m, n = size(A)
-    m != n && throw(DimensionMismatch("Matrix must be square"))
-
-    parent = zeros(Ti, n)
+    par = zeros(Ti, n)
     ancestor = zeros(Ti, n)
 
-    for col in 1:n, p in nzrange(A, col)
+    _etree(A, par, ancestor)
+    return par
+end
+
+# (essentially) nonallocating verion of above
+function _etree(A::SparseMatrixCSC{Tv, Ti}, par::Vector{Ti}, ancestor::Vector{Ti}) where {Tv <: AbstractFloat, Ti <: Integer}
+    for col::Ti in 1:n, p in nzrange(A, col) #A.colptr[col]:(A.colptr[col+1]-1)
         i = rowvals(A)[p]
-        while i != 0 && i < col
+        while !iszero(i) && i < col
             i_next = ancestor[i]
             ancestor[i] = col
-            if i_next == 0
-                parent[i] = col
+            if iszero(i_next)
+                par[i] = col
             end
             i = i_next
         end
     end
-
-    return parent
+    return nothing
 end
