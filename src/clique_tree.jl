e7802c70b17a8d3b56313d3cf025ab24ae6e6893
Theo Diamandis <tjdiamandis@gmail.com>
2021-06-24 10:45:54 -0400

    moving stuff around

--- src/clique_tree.jl
+++ src/clique_tree.jl
@@ -6,37 +6,6 @@ struct CliqueTree
 end
 
 
-"""
-    etree(A)
-Compute the elimination tree of a symmetric sparse matrix `A` from `triu(A)`.
-"""
-function etree(A::SparseMatrixCSC{Tv, Ti}) where {Tv <: AbstractFloat, Ti <: Integer}
-    !issymmetric(A) && throw(ArgumentError("Matrix must be symmetric"))
-    m, n = size(A)
-    par = zeros(Ti, n)
-    ancestor = zeros(Ti, n)
-
-    _etree(A, par, ancestor)
-    return par
-end
-
-# (almost) nonallocating verion of above
-function _etree(A::SparseMatrixCSC{Tv, Ti}, par::Vector{Ti}, ancestor::Vector{Ti}) where {Tv <: AbstractFloat, Ti <: Integer}
-    for col::Ti in 1:n, p in nzrange(A, col) #A.colptr[col]:(A.colptr[col+1]-1)
-        i = rowvals(A)[p]
-        while !iszero(i) && i < col
-            i_next = ancestor[i]
-            ancestor[i] = col
-            if iszero(i_next)
-                par[i] = col
-            end
-            i = i_next
-        end
-    end
-    return nothing
-end
-
-
 # Input:
 #   L       : sparse lower triangular matrix that rep chordal graph
 #   cliques : vector of cliques
@@ -122,4 +91,35 @@ function get_postordering(root, par, child)
     postordering = sort(1:m, by=x->visit_order[x])
 
     return postordering
+end
+
+
+"""
+    etree(A)
+Compute the elimination tree of a symmetric sparse matrix `A` from `triu(A)`.
+"""
+function etree(A::SparseMatrixCSC{Tv, Ti}) where {Tv <: AbstractFloat, Ti <: Integer}
+    !issymmetric(A) && throw(ArgumentError("Matrix must be symmetric"))
+    m, n = size(A)
+    par = zeros(Ti, n)
+    ancestor = zeros(Ti, n)
+
+    _etree(A, par, ancestor)
+    return par
+end
+
+# (almost) nonallocating verion of above
+function _etree(A::SparseMatrixCSC{Tv, Ti}, par::Vector{Ti}, ancestor::Vector{Ti}) where {Tv <: AbstractFloat, Ti <: Integer}
+    for col::Ti in 1:n, p in nzrange(A, col) #A.colptr[col]:(A.colptr[col+1]-1)
+        i = rowvals(A)[p]
+        while !iszero(i) && i < col
+            i_next = ancestor[i]
+            ancestor[i] = col
+            if iszero(i_next)
+                par[i] = col
+            end
+            i = i_next
+        end
+    end
+    return nothing
 end
