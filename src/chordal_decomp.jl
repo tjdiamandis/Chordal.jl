0edbb4e30b75822383a6f1de738254388a87b19a
Theo Diamandis <tjdiamandis@gmail.com>
2021-06-28 10:38:46 -0400

    change interface

--- src/chordal_decomp.jl
+++ src/chordal_decomp.jl
@@ -35,19 +35,24 @@ function get_selectors(input_mat::SparseMatrixCSC; verbose=true, ret_cliques=tru
     preprocess!(input_mat)
 
     sp = sparsity_pattern(input_mat)
-    P, L = get_chordal_extension(sp; verbose=verbose)
-    P = build_perm_matrix(P)
+    # TODO: make this work for matrices that are block diagonal (under reordering)
+    if is_separable(sp)
+        error(ArgumentError("Input matrix should not be block diagonal; decompose the blocks separately."))
+    end
+
+    # L is the chordal extension of sp under reordering perm
+    perm, iperm, L = get_chordal_extension(sp; verbose=verbose)
 
     cliques = get_cliques(L)
     cg = generate_clique_graph(cliques, n)
     merge_cliques!(cg; verbose=verbose)
 
     if ret_cliques
-        Cℓs = get_cliques(cg)
-        Tℓs = make_selectors_from_cliques(Cℓs, n)
-        return P, Cℓs, Tℓs
+        Cls = get_cliques(cg)
+        Tls = make_selectors_from_cliques(Cls, n)
+        return perm, iperm, Tls, Cls
     end
 
-    Tℓs = make_selectors_from_clique_graph(cg, n)
-    return P, Tℓs
+    Tls = make_selectors_from_clique_graph(cg, n)
+    return perm, iperm, Tls
 end
