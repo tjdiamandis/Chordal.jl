227b938b4591ad77b23481676856ded9249d6dc4
Theo Diamandis <tjdiamandis@gmail.com>
2021-10-24 22:06:34 -0400

    add docs

--- src/completion.jl
+++ src/completion.jl
@@ -208,7 +208,17 @@ function maxdet_completion_factors(A::SparseMatrixCSC{Tv, Ti}) where {Tv <: Abst
 end
 
 
-# FIXME: errors on higher rank tests
+"""
+    minrank_completion(A::SparseMatrixCSC{Tv, Ti}) where {Tv <: AbstractFloat, Ti <: Integer}
+
+Returns the cholesky factor of the minimum rank completion (`A_complete = Y*Yᵀ`)
+of chordal sparse matrix `A` using Algorithm 2 in [Sun15]. This algorithm uses
+the supernodal elimination tree of `A` and, therefore, BLAS level 3 operations.
+
+#Reference
+[Sun15] [Decomposition Methods for Semidefinite Optimization (PhD Thesis)](https://escholarship.org/content/qt1cv6981p/qt1cv6981p.pdf)
+by Yifan Sun
+"""
 function minrank_completion(A::SparseMatrixCSC{Tv, Ti}) where {Tv <: AbstractFloat, Ti <: Integer}
     !issymmetric(A) && error(ArgumentError("A must be symmetric"))
     n = size(A, 1)
