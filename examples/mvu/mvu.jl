f7ff2334978171eff69e2f0f4547aaf37346f7c8
Theo Diamandis <tjdiamandis@gmail.com>
2021-10-27 13:14:54 -0400

    add MVU example

---
+++ examples/mvu/mvu.jl
@@ -0,0 +1,47 @@
+using Chordal
+using JuMP, Hypatia
+using SparseArrays, LinearAlgebra, Random
+using GLMakie
+
+include("utils.jl")
+
+# Construct Dataset
+n, k = 20, 2
+x, theta = swissroll(n)
+show_data(x, theta)
+
+
+# Construct problem data
+V, VA, b = get_constraint_matrices(x, k)
+C = V'*V
+perm, iperm, Tls, cliques = get_selectors(sparsity_pattern([VA..., C]); verbose=false, ret_cliques=true, perm=nothing)
+sp = Chordal.get_sparsity_pattern_from_cliques(cliques)
+sp_inds = zip(findnz(sp)[1:2]...)
+
+# Construct Model
+model_c = Model(Hypatia.Optimizer)
+Z = @variable(model_c, Z[i=1:n-1, j=1:n-1; (i,j) in sp_inds])
+
+# Constraints
+@objective(model_c, Max, Chordal.tr(C, Z))
+for i in 1:length(b)
+    @constraint(model_c, Chordal.tr(VA[i], Z) == b[i])
+end
+for (p, cl) in enumerate(cliques)
+    len_cl = length(cl)
+    Zp = @variable(model_c, [1:len_cl, 1:len_cl], base_name="Z$p")
+    for i in 1:len_cl, j in 1:len_cl
+        Zp[i,j] = Z[cl[i], cl[j]]
+    end
+    @constraint(model_c, Zp in PSDCone())
+end
+optimize!(model_c)
+
+# Reconstruct solution
+Z_ = Chordal.reconstruct_from_sparse_varref(Z, n-1)
+Z_ = 0.5*(Z_ + Z_') # Deals with numerical error
+
+Z_factor = Chordal.minrank_completion(Z_)
+W, sv, _ = svd(V*Z_factor)
+yc = Diagonal(sv[1:3])*W[:, 1:3]'
+show_data(yc)
---
+++ examples/mvu/utils.jl
@@ -0,0 +1,108 @@
+# Generate swissroll dataset
+function swissroll(n)
+    # NOTE: these could be randomized too
+    theta = 1.5 .* pi .+ 1.5 .* pi .* LinRange(0, 1, n);
+    z = [iseven(i) ? 1.0 : 0.0 for i in 1:n]
+    return [theta' .* [cos.(theta'); sin.(theta')]; z'], theta;
+end
+
+function show_data(x)
+    fig = Figure(resolution=(1000,1000))
+    ax = LScene(fig)
+    GLMakie.scatter!(Point3f0.(eachcol(x)), color = theta, markersize=500)
+    # GLMakie.scatter!(Point3f0.(eachcol(x)), color = theta)
+    fig[1, 1] = ax
+    display(fig)
+end
+
+function show_data(x, theta)
+    fig = Figure(resolution=(1000,1000))
+    ax = LScene(fig)
+    GLMakie.scatter!(Point3f0.(eachcol(x)), color = theta, markersize=500)
+    # GLMakie.scatter!(Point3f0.(eachcol(x)), color = theta)
+    fig[1, 1] = ax
+    display(fig)
+end
+
+
+# Constructs Euclidean Distance Matrix given vectors x = [x_1 ... x_n]
+function edm(x)
+    G = x' * x
+    dg = diag(G)
+    return -2*G + dg*ones(size(G,1))' + ones(size(G,1))*dg'
+end
+
+
+# Given edge weights E, Eij = d(xi, xj), finds k nearest neighbors
+function knn(E, k)
+    nn = [Vector{Int}(undef,0) for _ in 1:size(E, 1)]
+    for i in 1:size(E, 1)
+        nn[i] = partialsortperm(@view(E[:,i]), 1:k+1)
+    end
+    return nn
+end
+
+
+# from nearest neighbors, constructs the MVU edge list
+function knn_edges(nn, clique=true)
+    k = length(nn) - 1
+    E = CartesianIndex{2}[]
+
+    for i in 1:k+1
+        for j in i+1:k+1
+            i == j && continue
+            !clique && i > 1 && continue
+
+            push!(E, CartesianIndex(nn[i], nn[j]))
+        end
+    end
+    return E
+end
+
+
+# Returns edge list for k nearest neighbors given vectors X
+function edges(x, k, clique=true)
+    nn = knn(edm(x), k)
+    return reduce(vcat, [knn_edges(nn[i], clique) for i in 1:length(nn)])
+end
+
+
+# Constructs V matrix (basis)
+function get_V(n)
+    return spdiagm(n, n-1, 0 => ones(n-1), -1 => -ones(n-1))
+end
+
+
+# Constructs constrain matrices as described in section 3.1
+function get_constraint_matrices(x, k)
+    n = size(x, 2)
+    D = edm(x)
+
+    E = edges(x, k)
+    Ei = [CartesianIndex(e[1], e[1]) for e in E]
+    Ej = [CartesianIndex(e[2], e[2]) for e in E]
+
+    b = [D[e] for e in E]
+    A = [sparse(
+            [e[1], e[2], e[1], e[2]],
+            [e[1], e[2], e[2], e[1]],
+            [1, 1, -1, -1], n, n
+         ) for e in E]
+    V = get_V(n)
+    VA = [V'*Ai*V for Ai in A]
+
+    return V, VA, b
+end
+
+
+# Returns a SparseMatrixCSC with values of Zref
+#   Zref is a SparseAxisArray, which functions like a dictionary
+#   - Requires optimizer to have been run
+function reconstruct_from_sparse_varref(Zref, n)
+    Zcv = value.(Zref)
+    Z_uncomp = spzeros(Float64, n, n)
+    for ind in eachindex(Zcv)
+        Z_uncomp[ind[1], ind[2]] = Zcv[ind]
+    end
+    return Z_uncomp
+end
