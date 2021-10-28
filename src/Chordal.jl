840acf1cd35f23539f38b209ac8f896051389f76
Theo Diamandis <tjdiamandis@gmail.com>
2021-10-28 15:22:39 -0400

    export min rank completion

--- src/Chordal.jl
+++ src/Chordal.jl
@@ -20,7 +20,7 @@ include("jump.jl")
 export sparsity_pattern, get_chordal_extension
 export generate_clique_graph, merge_cliques!, get_cliques
 export get_selectors, make_selectors_from_cliques, make_selectors_from_clique_graph
-export maxdet_completion
+export maxdet_completion, minrank_completion
 export build_constraints_lmi!
 export get_sparsity_pattern_from_cliques, reconstruct_from_sparse_varref
 export edm_completion
